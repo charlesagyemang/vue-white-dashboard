57ff41d03ce038964b0fe8ad94b589c8c3f0233b
Work <micnkru@gmail.com>
2020-09-22 18:11:53 +0000

    Feat: Add Model for adding a car

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -63,24 +63,68 @@
         <br>
       <div class="row">
         <div class="col-md-4">
-            <base-button block type="primary" class=" mb-3" @click="modals.modal1 = true">
-                Default
+            <base-button block type="default" class=" mb-3" @click="modals.modal1 = true">
+                Add A Car
             </base-button>
-            <modal :show.sync="modals.modal1">
-                <h6 slot="header" class="modal-title" id="modal-title-default">Type your modal title</h6>
+            <modal :show.sync="modals.modal1"
+                   body-classes="p-0"
+                   modal-classes="modal-dialog-centered modal-sm">
+                   <div v-show="validationError">
+                     <base-alert type="danger" dismissible>
+                         <span class="alert-inner--icon"><i class="ni ni-like-2"></i></span>
+                         <span class="alert-inner--text"><strong>Error!</strong> Please Fill Out All Required Fields</span>
+                         <button type="button" class="close">
+                             <span aria-hidden="true">&times;</span>
+                         </button>
+                     </base-alert>
+                   </div>
 
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                    Consonantia, there live the blind texts. Separated they live in Bookmarksgrove
-                    right at the coast of the Semantics, a large language ocean.</p>
-                <p>A small river named Duden flows by their place and supplies it with the necessary
-                    regelialia. It is a paradisematic country, in which roasted parts of sentences
-                    fly into your mouth.</p>
-
-                <template slot="footer">
-                    <base-button type="primary">Save changes</base-button>
-                    <base-button type="link" class="ml-auto" @click="modals.modal1 = false">Close
-                    </base-button>
-                </template>
+                <card type="secondary" shadow
+                      header-classes="bg-white pb-5"
+                      body-classes="px-lg-5 py-lg-5"
+                      class="border-0">
+                    <template>
+                        <div class="text-center text-muted mb-4">
+                            <small>Perform Action</small>
+                        </div>
+                        <form role="form">
+                            <base-input
+                                        v-model="createOwner.username"
+                                        type="text"
+                                        placeholder="Full Name"
+                                        addon-left-icon="ni ni-single-02">
+                            </base-input>
+                            <base-input alternative
+                                        v-model="createOwner.email"
+                                        class="mb-3"
+                                        type="email"
+                                        placeholder="Email"
+                                        addon-left-icon="ni ni-email-83">
+                            </base-input>
+                            <base-input alternative
+                                        v-model="createOwner.phoneNumber"
+                                        type="text"
+                                        placeholder="Phone Number"
+                                        addon-left-icon="ni ni-mobile-button">
+                            </base-input>
+                            <base-input alternative
+                                        v-model="createOwner.address"
+                                        type="text"
+                                        placeholder="Address"
+                                        addon-left-icon="ni ni-square-pin">
+                            </base-input>
+                            <base-input alternative
+                                        v-model="createOwner.country"
+                                        type="text"
+                                        placeholder="Country Of Origin"
+                                        addon-left-icon="ni ni-world-2">
+                            </base-input>
+                            <div class="text-center">
+                                <base-button @click="handleCreateOwner" type="default" class="my-4">Submit</base-button>
+                            </div>
+                        </form>
+                    </template>
+                </card>
             </modal>
         </div>
         <div class="col-md-4">
@@ -205,6 +249,30 @@ export default {
         country: '',
         other: {},
       },
+      carForm: {
+        modelName: '', //KIA PICANTO
+        modelYear: '', //2008
+        color: '', // RED || RED & YELLOW
+        carStatus: '', // BOUGHT, SHIPPED, SETTING_UP, PROBATION, WORKING, SOLD, GIVEN_OUT
+        carWorkingCity: '', //ACCRA GHANA
+        carOwner: '', // OWNERID
+        carOwners: [], // LIST OF OWNERS NOT JUST IDS
+        currentDriver: '', // FULL DRIVER DETAILS
+        allDrivers: [], // LIST OF DRIVERS NOT JUST IDS
+        carType: '', // UT-TYPE, TRUCK-SIZE
+        serviceType: '', // UBER-SERVICES, TAXI-SERVICES, DUMP-TRACK-SEVICES
+        dateResigtered: '', // 22ND MARCH 2021
+        carNumber: '', // GS 4567 - 20
+        insuranceDetails: [], // [{year: '', type: '', status: '', amount: ''}],
+        roadWorthyDetails: [], // [{year: '', period: '', amount: '', renewalDate: ''}]
+        incomeTaxDetails: [], // [{year: '', period: '', amount: '', renewalDate: ''}]
+        costOfAquiring: '', // $10,000
+        costOfShipping: '', // $890
+        costOfClearing: '', // GHC 39493
+        costOfSettingUp: '', // GHC 7,000
+        imageUrl: '',
+        other: {},
+      },
       validationError: false,
     }
   },
@@ -213,7 +281,33 @@ export default {
       const noneIsEmpty = this.validateBody(this.createOwner);
       if (noneIsEmpty) {
         console.log("noneIsEmpty", "Go Agead Create Owner");
+        // CREATE OWNER
+        console.log(JSON.stringify(this.createOwner));
       } else {
+        alert("Error!! Please Fill All Required Fields")
+        this.validationError = true;
+      }
+    },
+    handleCreateCar() {
+      // required:
+      const requiredBody = {
+        modelName: this.carForm.modelName,
+        modelYear: this.carForm.modelYear,
+        color: this.carForm.color,
+        carStatus: this.carForm.carStatus,
+        carOwner: this.carForm.carOwner,
+        carType: this.carForm.carType,
+        costOfAquiring: this.carForm.costOfAquiring,
+      }
+
+      const noneIsEmpty = this.validateBody(requiredBody);
+
+      if (noneIsEmpty) {
+        console.log("noneIsEmpty", "Go Ahead Create A Car");
+        // CREATE A Car
+        console.log(JSON.stringify(this.carForm));
+      } else {
+        alert("Error!! Please Fill All Required Fields")
         this.validationError = true;
       }
     },
