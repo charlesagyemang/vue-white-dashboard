b101be66de940e76da7775ecc01dc91b5c42ff61
Work <micnkru@gmail.com>
2020-09-23 14:21:19 +0000

    Feat: init setup

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <base-header class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
-                     style="min-height: 600px; background-image: url(img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
+                     style="min-height: 600px; background-image: url(/img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
             <!-- Mask -->
             <span class="mask bg-dark opacity-8"></span>
             <!-- Header container -->
